a6d527bf69d7717aeb05aeb15ecdca028745456d
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-12-23 23:30:02 +0900

    🐛 Fix: start.sh에 필요한 환경변수 깃허브 액션에서 사용할 수 있도록 코드 수정
    fix: start.sh에 필요한 환경변수 깃허브 액션에서 사용할 수 있도록 코드 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/app
 
+# .env 파일 로드
+export $(cat /home/ubuntu/app/codedeploy.env | xargs)
+
 # 도커 이미지 풀
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker pull $ECR_REGISTRY:$IMAGE_TAG
