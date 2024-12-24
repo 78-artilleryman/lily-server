5ca7c259bf7c64109e2f9d7ec9a469fc558f422f
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2024-12-25 01:34:57 +0900

    ♻️ Refactor:  중복되는 작업 명령어 삭제
    refactor:  중복되는 작업 명령어 삭제

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,9 +8,5 @@ export $(cat /home/ubuntu/app/codedeploy.env | xargs)
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker pull $ECR_REGISTRY:$IMAGE_TAG
 
-# 기존 컨테이너 중지 및 삭제
-docker stop $(docker ps -a -q) 2>/dev/null || true
-docker rm $(docker ps -a -q) 2>/dev/null || true
-
 # 새 컨테이너 실행
 docker run -d -p 3001:3001 --name app $ECR_REGISTRY:$IMAGE_TAG 
