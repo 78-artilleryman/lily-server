8414139e748a4ebdd3b187d8e13af44b4fdd3a67
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-01-16 16:15:09 +0900

    ✨ Feat: 구글 로그인 데이터베이스 저장 로직 작성
    feat: 구글 로그인 데이터베이스 저장 로직 작성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,9 +9,22 @@ export class AuthService {
     if (!req.user) {
       return 'No user from Google';
     }
-    return {
-      message: 'User information from Google',
-      user: req.user,
-    };
+    const { email, name } = req.user;
+
+    // 사용자 정보 저장 (이미 존재하면 업데이트 또는 무시)
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!existingUser) {
+      return this.prisma.user.create({
+        data: {
+          email,
+          name,
+        },
+      });
+    }
+
+    return existingUser; // 이미 등록된 사용자 반환
   }
 }
