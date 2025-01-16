6522bc29f488940d763bd716c1191f0fd2276cbf
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-01-16 14:32:35 +0900

    ✨ Feat: prisma module, service 생성
    feat: prisma module, service 생성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, PrismaModule],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async googleLogin(req: any) {
     if (!req.user) {
       return 'No user from Google';
