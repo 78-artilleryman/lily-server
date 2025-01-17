d78c8ddd1e6dd728a131a3e2a5b951e3bbc9d3b3
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-01-17 22:58:27 +0900

    🐛 Fix: test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,14 +7,14 @@ export class UserService {
 
   // 사용자 생성
   async create(data: any) {
-    return this.prisma.user.create({
+    return await this.prisma.user.create({
       data,
     });
   }
 
   // 소셜 ID로 사용자 검색
   async findBySocialId(provider: string, providerId: string) {
-    return this.prisma.user.findUnique({
+    return await this.prisma.user.findUnique({
       where: {
         provider_providerId: {
           provider,
@@ -26,7 +26,7 @@ export class UserService {
 
   // 사용자 이메일로 검색 (추가적인 필요 로직)
   async findByEmail(email: string) {
-    return this.prisma.user.findUnique({
+    return await this.prisma.user.findUnique({
       where: { email },
     });
   }
